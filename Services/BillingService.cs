42a2da1c546ae3a2cfa8f687032945eabaa5b951
Nik <nikitozer25@gmail.com>
2023-08-24 21:49:10 +0700

    Generated .proto class

---
+++ Services/BillingService.cs
@@ -0,0 +1,26 @@
+﻿using Billing;
+using Grpc.Core;
+
+namespace Billing
+{
+    public class BillingService : Billing.BillingBase
+    {
+        public override Task<Response> CoinsEmission(EmissionAmount request, ServerCallContext context)
+        {
+            return base.CoinsEmission(request, context);
+        }
+        public override Task<Response> MoveCoins(MoveCoinsTransaction request, ServerCallContext context)
+        {
+            return base.MoveCoins(request, context);
+        }
+        public override Task ListUsers(None request, IServerStreamWriter<UserProfile> responseStream, ServerCallContext context)
+        {
+            return base.ListUsers(request, responseStream, context);
+        }
+        public override Task<Coin> LongestHistoryCoin(None request, ServerCallContext context)
+        {
+            return base.LongestHistoryCoin(request, context);
+        }
+    }
+
+}
