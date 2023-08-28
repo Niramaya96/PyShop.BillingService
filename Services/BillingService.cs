f531c352bc889c7d97b2c64f0ed9d1947676d5f4
Nik <nikitozer25@gmail.com>
2023-08-28 18:34:54 +0700

    .

--- Services/BillingService.cs
+++ Services/BillingService.cs
@@ -1,26 +1,46 @@
 ﻿using Grpc.Core;
+using PyShop.BillingService.Services;
 
 namespace Billing
 {
     public class BillingService : Billing.BillingBase
     {
-        private List<UserProfile> Users = new List<UserProfile>()
-        { new UserProfile() { Name = "Boris",Amount=0},
-          new UserProfile() { Name="Maria",Amount = 0},
-          new UserProfile() {Name="Oleg",Amount=0}};
-        public override Task<Response> CoinsEmission(EmissionAmount request, ServerCallContext context)
+        private List<User> Users = new List<User>()
+        { new User("boris",0,5000),
+          new User("maria",0,1000),
+          new User("oleg",0,800) 
+        };
+          
+
+        private List<Coin> Coins = new List<Coin>();
+
+        public override async Task<Response> CoinsEmission(EmissionAmount request, ServerCallContext context)
         {
-            return base.CoinsEmission(request, context);
+           for (int i = 0; i < request.Amount; i++)
+           {
+                Coins.Add(new Coin() { Id=i,History="CoinEmission"});
+           }
+            return await Task.FromResult(new Response() { Status = Response.Types.Status.Ok, Comment = "EmissionComplete" });
         }
         public override Task<Response> MoveCoins(MoveCoinsTransaction request, ServerCallContext context)
         {
+            var dst_user = Users.Find(u => u.Name == request.DstUser);
+            var src_user = Users.Find(u => u.Name == request.SrcUser);
+            if (dst_user != null && src_user != null)
+            {
+                if (true)
+                {
+
+                }
+            }
             return base.MoveCoins(request, context);
         }
         public override async Task ListUsers(None request, IServerStreamWriter<UserProfile> responseStream, ServerCallContext context)
         {
-            foreach (UserProfile user in Users)
-            { 
-                await responseStream.WriteAsync(user);
+            foreach (User user in Users)
+            {
+                var userProfile = new UserProfile() { Name = user.Name, Amount = user.Amount };
+                await responseStream.WriteAsync(userProfile);
             }
         }
         public override Task<Coin> LongestHistoryCoin(None request, ServerCallContext context)
---
+++ Services/User.cs
@@ -0,0 +1,17 @@
+﻿using Billing;
+
+namespace PyShop.BillingService.Services
+{
+    public class User
+    {
+        public User(string name,int amount,int rating)
+        {
+            Name = name;
+            Amount = amount;
+            Rating = rating;
+        }
+        public string Name { get; set; } 
+        public int Amount { get; set; }
+        public int Rating { get; set; }
+    }
+}
