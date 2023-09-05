0daa55cbbe149847f536742df6a74d4d1de6b203
Nik <nikitozer25@gmail.com>
2023-09-05 18:54:21 +0700

    /

--- Services/BillingService.cs
+++ Services/BillingService.cs
@@ -5,22 +5,33 @@ namespace Billing
 {
     public class BillingService : Billing.BillingBase
     {
-        private List<User> Users = new List<User>()
-        { new User("boris",0,5000),
-          new User("maria",0,1000),
-          new User("oleg",0,800) 
-        };
-          
-
-        private List<Coin> Coins = new List<Coin>();
+        private List<User> Users;
+        public BillingService()
+        {
+          Users = new List<User>()
+           {
+              new User("boris",5000),
+              new User("maria",1000),
+              new User("oleg",800)
+           };
+        }
 
         public override async Task<Response> CoinsEmission(EmissionAmount request, ServerCallContext context)
         {
-           for (int i = 0; i < request.Amount; i++)
-           {
-                Coins.Add(new Coin() { Id=i,History="CoinEmission"});
+           
+           var totalRating = Users.Select(x => x.Rating).Sum();
+            var lastIndex = 0;
+           for(var i = 0; i < Users.Count; i++) 
+           { 
+                var user = Users[i];
+                var coinsCount = (int)Math.Round((double)user.Rating / totalRating * request.Amount);
+                for (int j = 0; j < coinsCount; j++)
+                {
+                    user.Coins.Add(new Coin() { Id = lastIndex, History = $"Emission:{user.Name}" });
+                    lastIndex++;
+                }
            }
-            return await Task.FromResult(new Response() { Status = Response.Types.Status.Ok, Comment = "EmissionComplete" });
+            return await Task.FromResult(new Response() { Status = Response.Types.Status.Ok, Comment = $"EmissionComplete" });
         }
         public override Task<Response> MoveCoins(MoveCoinsTransaction request, ServerCallContext context)
         {
--- Services/User.cs
+++ Services/User.cs
@@ -4,14 +4,14 @@ namespace PyShop.BillingService.Services
 {
     public class User
     {
-        public User(string name,int amount,int rating)
+        public User(string name,int rating)
         {
             Name = name;
-            Amount = amount;
             Rating = rating;
         }
         public string Name { get; set; } 
-        public int Amount { get; set; }
+        public int Amount { get => Coins.Count;}
         public int Rating { get; set; }
+        public List<Coin> Coins { get; set; } = new List<Coin>();
     }
 }
